27b49e43c6db72897e2f6e922c1f9851f9ce56bd
Ranbir <71406920+ranbir7@users.noreply.github.com>
2025-05-30 23:08:14 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="lib2ran",
+    version="1.0.0",
+    description="Library Genesis Book Downloader by Ran7",
+    author="Ranbir",
+    packages=["lib2ran"],
+    install_requires=[
+        "libgen-api",
+        "inquirer",
+        "requests"
+    ],
+    entry_points={
+        "console_scripts": [
+            "lib2ran = lib2ran.__main__:main"
+        ]
+    },
+    python_requires='>=3.7',
+)
